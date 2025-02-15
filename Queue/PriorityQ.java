dff454f0aaa75970e4be5fe506c9a50911e9c45c
bagashu2525 <bagasmita25@gmal.com>
2025-02-15 16:17:00 +0530

    Priority Queue

--- Queue/PriorityQ.java
+++ Queue/PriorityQ.java
@@ -3,7 +3,10 @@ import java.util.PriorityQueue;
 
 public class PriorityQ {
     public static void main(String[] args) {
+        //create
         PriorityQueue<Integer> pq = new PriorityQueue<>(Arrays.asList(30, 10, 20, 5));
+
+        //traversing
         System.out.println("Iterating using for-each:");
         for (int num : pq) {
             System.out.println(num); // Order is NOT guaranteed
